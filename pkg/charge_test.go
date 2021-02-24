5df9efd037a16dda63d74c76eafc5ef6891cf3f5
Edosa Kelvin <edosakelvin@gmail.com>
2021-02-24 20:36:43 +0100

    Add better tests for charge service

--- pkg/charge_test.go
+++ pkg/charge_test.go
@@ -1,64 +1,107 @@
 package paystack
 
 import (
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestChargeServiceCreate(t *testing.T) {
+func TestChargeCardAndAuthorization(t *testing.T) {
+	assert := assert.New(t)
+	// Card does not require validation
+	card := Card{
+		Number:      "4084084084084081",
+		CVV:         "408",
+		ExpiryMonth: fmt.Sprintf("%d", time.Now().Month()),
+		ExpiryYear:  fmt.Sprintf("%d", time.Now().Year()),
+	}
+	charge := &ChargeRequest{
+		Email:  "test@user.com",
+		Amount: 10000,
+		Card:   &card,
+	}
+
+	r, err := c.Charge.Create(charge)
+	assert.NoError(err)
+	assert.Equal(r["status"], "success")
+
+	auth := r["authorization"].(map[string]interface{})
+	authCode := auth["authorization_code"].(string)
+	reusable := auth["reusable"].(bool)
+
+	assert.True(reusable, "Card instruments should always be reusable")
+
+	// Use the auth code to perform another charge
+	charge = &ChargeRequest{
+		Email:             "test@user.com",
+		Amount:            10000,
+		AuthorizationCode: authCode,
+	}
+
+	r, err = c.Charge.Create(charge)
+	assert.NoError(err)
+	assert.Equal(r["status"], "success")
+}
+
+func TestChargeBank(t *testing.T) {
+	assert := assert.New(t)
+
 	bankAccount := BankAccount{
 		Code:          "057",
 		AccountNumber: "0000000000",
 	}
 
 	charge := ChargeRequest{
-		Email:    "your_own_email_here@gmail.com",
+		Email:    "test@user.com",
 		Amount:   10000,
 		Bank:     &bankAccount,
-		Birthday: "1999-12-31",
+		Birthday: "1993-02-24",
 	}
 
-	resp, err := c.Charge.Create(&charge)
-	if err != nil {
-		t.Errorf("Create Charge returned error: %v", err)
-	}
+	r, err := c.Charge.Create(&charge)
+	assert.NoError(err)
+	assert.NotEmpty(r["reference"])
+	assert.NotEmpty(r["status"])
 
-	if resp["reference"] == "" {
-		t.Error("Missing transaction reference")
+	for {
+		switch r["status"] {
+		case "success":
+			return
+		case "send_otp":
+			r, err = c.Charge.SubmitOTP("123456", r["reference"].(string))
+			assert.NoError(err)
+			assert.NotEmpty(r["reference"])
+			assert.NotEmpty(r["status"])
+		default:
+			t.Errorf(`Expected status of success or send_otp, got %s`, r["status"])
+		}
 	}
 }
 
 func TestChargeServiceCheckPending(t *testing.T) {
+	assert := assert.New(t)
+
 	bankAccount := BankAccount{
 		Code:          "057",
 		AccountNumber: "0000000000",
 	}
 
 	charge := ChargeRequest{
-		Email:    "your_own_email_here@gmail.com",
+		Email:    "test@user.com",
 		Amount:   10000,
 		Bank:     &bankAccount,
 		Birthday: "1999-12-31",
 	}
 
-	resp, err := c.Charge.Create(&charge)
-	if err != nil {
-		t.Errorf("Create charge returned error: %v", err)
-	}
+	r, err := c.Charge.Create(&charge)
+	assert.NoError(err)
+	assert.NotEmpty(r["reference"])
+	assert.NotEmpty(r["status"])
 
-	if resp["reference"] == "" {
-		t.Error("Missing charge reference")
-	}
-
-	resp2, err := c.Charge.CheckPending(resp["reference"].(string))
-	if err != nil {
-		t.Errorf("Check pending charge returned error: %v", err)
-	}
-
-	if resp2["status"] == "" {
-		t.Error("Missing charge pending status")
-	}
-
-	if resp2["reference"] == "" {
-		t.Error("Missing charge pending reference")
-	}
+	r, err = c.Charge.CheckPending(r["reference"].(string))
+	assert.NoError(err)
+	assert.NotEmpty(r["status"])
+	assert.NotEmpty(r["reference"])
 }
