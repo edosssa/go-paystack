3c3d1521e97814d931f277b28dac33b891a4084b
Edosa Kelvin <edosakelvin@gmail.com>
2021-02-24 20:34:48 +0100

    Refactor tests and use testify

--- pkg/paystack_test.go
+++ pkg/paystack_test.go
@@ -1,6 +1,10 @@
 package paystack
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var c *Client
 
@@ -10,47 +14,23 @@ func init() {
 }
 
 func TestResolveCardBIN(t *testing.T) {
-	resp, err := c.ResolveCardBIN(59983)
-	if err != nil {
-		t.Error(err)
-	}
-	if _, ok := resp["bin"]; !ok {
-		t.Errorf("Expected response to contain bin")
-	}
+	assert := assert.New(t)
+	r, err := c.ResolveCardBIN(59983)
+	assert.NoError(err)
+	assert.NotEmpty(r["bin"])
 }
 
 func TestCheckBalance(t *testing.T) {
-	resp, err := c.CheckBalance()
-	if err != nil {
-		t.Error(err)
-	}
-	if _, ok := resp["currency"]; !ok {
-		t.Errorf("Expected response to contain currency")
-	}
-
-	if _, ok := resp["balance"]; !ok {
-		t.Errorf("Expected response to contain balance")
-	}
+	assert := assert.New(t)
+	balances, err := c.CheckBalance()
+	assert.NoError(err)
+	// Ideally, there are two balances, NGN and USD
+	assert.GreaterOrEqual(len(balances), 2)
 }
 
 func TestSessionTimeout(t *testing.T) {
-	resp, err := c.GetSessionTimeout()
-	if err != nil {
-		t.Error(err)
-	}
-	if _, ok := resp["payment_session_timeout"]; !ok {
-		t.Errorf("Expected response to contain payment_session_timeout")
-	}
-
-	/*
-		// actual tests in the Paystack API console also fails. Likely a server error
-			resp, err = c.UpdateSessionTimeout(30)
-			if err != nil {
-				t.Error(err)
-			}
-
-			if _, ok := resp["payment_session_timeout"]; !ok {
-				t.Errorf("Expected response to contain payment_session_timeout")
-			}
-	*/
+	assert := assert.New(t)
+	r, err := c.GetSessionTimeout()
+	assert.NoError(err)
+	assert.NotNil(r["payment_session_timeout"])
 }
